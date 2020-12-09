7aa358791bdca9467e883f34abc930b12ab360fe
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-09 15:27:20 +0900

    Active Storageを導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :title
     validates :comment
     validates :address
-    # validates :image
+    validates :image
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX
 
-    has_mny :posts
+    has_many :posts
 end
