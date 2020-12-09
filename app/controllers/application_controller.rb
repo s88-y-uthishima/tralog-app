1da0d5c7e31e38bade9501c5c3907c9ee4ad57c5
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-09 12:35:01 +0900

    ログイン実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  # emailとpassword以外の値も保存できるように追記するため
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
