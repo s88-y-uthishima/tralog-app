347f0b22db1c4c24deb7ec3cc71d5eb8f7c0e238
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-10 14:37:22 +0900

    編集、削除機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_post, only: [:show, :edit, :update]
+  before_action :move_to_index, only: :edit
+  
   def index
     @posts = Post.includes(:user).order('created_at DESC')
   end
@@ -17,11 +21,35 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy if current_user.id == post.user_id
+    redirect_to root_path
   end
 
   private
   def post_params
     params.require(:post).permit(:title, :comment, :address, :image).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id != @post.user_id
   end
 end
